a459ff300684c12eff0d3ec81f2d53c5358291da
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-07 23:26:58 +0600

    updated code

--- index.js
+++ index.js
@@ -11,13 +11,21 @@ const loadAllPhones = async () => {
     // using async await
     const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
     const data = await response.json();
+    // if said 6 data then use this method
+    // displayAllPhone(data.data.slice(0, 6));
     displayAllPhone(data.data);
 }
 
-// another fun
+// another function
 
 const displayAllPhone = (phones) => {
     console.log(phones);
+}
+
+// show function
+
+const handleShowAll = () => {
+    console.log("Hello");
 }
 
 // spinner timing set and call loadAll function from here
