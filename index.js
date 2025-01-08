26623b2d27f6896ef47c54799d4ba83bc1c722d2
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-08 17:26:57 +0600

    added load all function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const loadAllPhones = async () => {
+const loadAllPhones = async (status) => {
     // spinner none/stop
     document.getElementById("spinner").style.display = "none";
     console.log("wow 3s gone");
@@ -12,8 +12,17 @@ const loadAllPhones = async () => {
     const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
     const data = await response.json();
     // if said 6 data then use this method
-    // displayAllPhone(data.data.slice(0, 6));
-    displayAllPhone(data.data);
+    displayAllPhone(data.data.slice(0, 6));
+    // displayAllPhone(data.data);
+
+    // console.log(status);
+
+    if (status == true) {
+        displayAllPhone(data.data);
+    }
+    else {
+        displayAllPhone(data.data.slice(0, 6));
+    }
 }
 
 // another function
@@ -25,7 +34,8 @@ const displayAllPhone = (phones) => {
 // show function
 
 const handleShowAll = () => {
-    console.log("Hello");
+    // console.log("Hello");
+    loadAllPhones(true);
 }
 
 // spinner timing set and call loadAll function from here
